369e23aefa8e2e7fa241bee0d7f29576628db5a1
vijay khatri <vijaykhatri22@gmail.com>
2018-02-27 11:52:28 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,10 +1,12 @@
 function callMe() {
+  var lyric;
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  lyric = "maybe";
   return lyric;
-  var lyric = "maybe";
+
 }
 
 
